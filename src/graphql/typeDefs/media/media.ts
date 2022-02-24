f572b7672b3bb0cef66db0077a964e5b7c2c10d0
Nick <nmclean@cycle.io>
2022-02-24 09:07:10 -0600

    v0.1.32 - implemented date time scalar

--- src/graphql/typeDefs/media/media.ts
+++ src/graphql/typeDefs/media/media.ts
@@ -3,8 +3,8 @@ import { gql } from "apollo-server-express";
 export const Media = gql`
   type Media @key(fields: "_id") {
     _id: ObjectID!
-    createdAt: String!
-    updatedAt: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
     path: String!
     mimetype: String!
     created_by: User!
